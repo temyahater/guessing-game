6d291d86924dbb8ae24ddb3da2d1bb23d00dc6e9
temyahater <55014811+temyahater@users.noreply.github.com>
2019-10-13 17:17:08 +0400

    feature : add new solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let midRange = Math.round((this.min + this.max) / 2);
+        return midRange;
     }
 
     lower() {
-
+        return this.max = this.guess();
     }
 
     greater() {
-
+        return this.min = this.guess();
     }
 }
 
